bf6e9e0eefed29c18d5ab5fbc1aa9d7ff46e8bb6
mrcaelumn <caelumnproperty@gmail.com>
2022-02-23 17:31:02 +0800

    update

--- few-shot-train.py
+++ few-shot-train.py
@@ -53,11 +53,11 @@ classes = 1
 
 dataset_name = "numbers"
 
+mode_colour = str(IMG_H) + "_rgb"
 if IMG_C == 1:
-    dataset_name = dataset_name + "_gray"
-else:
-    dataset_name = dataset_name + "_rgb"
-name_model = str(IMG_H)+"_"+dataset_name+"_few_shot_anomaly_detection"+"_"+str(meta_iters)
+    mode_colour = str(IMG_H) + "_gray"
+    
+name_model = mode_colour+"_"+dataset_name+"_few_shot_anomaly_detection"+"_"+str(meta_iters)
 g_model_path = "saved_model/"+name_model+"_g_model.h5"
 d_model_path = "saved_model/"+name_model+"_d_model.h5"
 
