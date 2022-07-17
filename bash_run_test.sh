5049609267ecbb5a264279185d04c45a6a347d32
mrcaelumn <caelumnproperty@gmail.com>
2022-07-18 01:02:36 +0800

    update

--- bash_run_test.sh
+++ bash_run_test.sh
@@ -47,12 +47,33 @@ for t in ${listDataset[@]}; do
     echo "Oops! I fell asleep for a couple seconds!"
 done
 
-
-
-
-
-
 echo "start training process for area dataset"
+for t in ${listDataset[@]}; do
+    version=1
+    echo "Start Program $t of version $version"
+    res_dir="result/$t""_v6/"
+    saved_model_dir="saved_model_0712/$t""_v1/"
+    # echo $res_dir
+    mkdir -p $res_dir
+    mkdir -p $saved_model_dir
+    # run programming
+    python3 main.py -dn $t -s 20 -nd 0 -bb "seresnext50" -m false -rd $res_dir -ted "test_data_v1" -trd "train_data" -eld "eval_data" -smd $saved_model_dir -rtd "target_data_new_smoothed_v2"
+
+    sleep 5
+    echo "Oops! I fell asleep for a couple seconds!"
+done
+
+
+
+
+
+
+
+
+
+
+
+echo "start training process for linear with gaussion filter 5x5 dataset"
 for t in ${listDataset[@]}; do
     version=1
     echo "Start Program $t of version $version"
@@ -69,7 +90,7 @@ for t in ${listDataset[@]}; do
 done
 
 
-echo "start training process for area dataset"
+echo "start training process for linear with gaussion filter 5x5 dataset"
 for t in ${listDataset[@]}; do
     version=1
     echo "Start Program $t of version $version"
@@ -80,6 +101,42 @@ for t in ${listDataset[@]}; do
     mkdir -p $saved_model_dir
     # run programming
     python3 main.py -dn $t -s 20 -nd 0 -bb "seresnext50" -m false -rd $res_dir -ted "test_data_v2" -trd "train_data" -eld "eval_data" -smd $saved_model_dir -rtd "target_data_new_smoothed"
+
+    sleep 5
+    echo "Oops! I fell asleep for a couple seconds!"
+done
+
+
+
+
+echo "start training process for linear with gaussion filter 3x3 dataset"
+for t in ${listDataset[@]}; do
+    version=2
+    echo "Start Program $t of version $version"
+    res_dir="result/$t""_v$version/"
+    saved_model_dir="saved_model/$t""_v$version/"
+    # echo $res_dir
+    mkdir -p $res_dir
+    mkdir -p $saved_model_dir
+    # run programming
+    python3 main.py -dn $t -s 20 -nd 0 -bb "seresnext50" -m true -rd $res_dir -ted "test_data_v1" -trd "train_data" -eld "eval_data" -smd $saved_model_dir -rtd "target_data_new_smoothed_v2"
+
+    sleep 5
+    echo "Oops! I fell asleep for a couple seconds!"
+done
+
+
+echo "start training process for linear with gaussion filter 3x3 dataset"
+for t in ${listDataset[@]}; do
+    version=2
+    echo "Start Program $t of version $version"
+    res_dir="result/$t""_v$version/"
+    saved_model_dir="saved_model/$t""_v$version/"
+    # echo $res_dir
+    mkdir -p $res_dir
+    mkdir -p $saved_model_dir
+    # run programming
+    python3 main.py -dn $t -s 20 -nd 0 -bb "seresnext50" -m false -rd $res_dir -ted "test_data_v2" -trd "train_data" -eld "eval_data" -smd $saved_model_dir -rtd "target_data_new_smoothed_v2"
 
     sleep 5
     echo "Oops! I fell asleep for a couple seconds!"
